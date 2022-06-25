4494e76c45b059359b19056156f1be6c6ad5032a
月海 <spectres@foxmail.com>
2022-06-25 22:12:42 +0800

    build: 初始化项目依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    // 解决 eslint 提示 defineProp undefined 的问题
+    "vue/setup-compiler-macros": true,
   },
   extends: [
     "plugin:vue/vue3-essential",
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,48 @@
+const path = require("path");
 const { defineConfig } = require("@vue/cli-service");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+
+const resolve = (dir) => path.join(__dirname, dir);
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/styles/mixin.scss";
+          @import "@/styles/utils.scss";
+        `,
+      },
+    },
+  },
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
+  chainWebpack: function (config) {
+    //！ exclude 是必要的，否则不会交由 svg-sprite-loader 处理
+    config.module
+      .rule("svg")
+      .exclude.add(resolve("src/assets/svg/icons"))
+      .end();
+    config.module
+      .rule("icons")
+      .test(/\.svg$/)
+      .include.add(resolve("src/assets/svg/icons"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]",
+      });
+  },
 });
